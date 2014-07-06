93224dafaffe722ec244a0c9267b88b03473110b
Zahar <zahar.zolotarev@gmain.com>
2014-07-06 15:09:58 +0300

    try move code to base guns class

--- src/donnu/zolotarev/SpaceShip/Weapons/Guns.java
+++ src/donnu/zolotarev/SpaceShip/Weapons/Guns.java
@@ -1,3 +1,35 @@
 package donnu.zolotarev.SpaceShip.Weapons;
 
-public class Guns {}
+import donnu.zolotarev.SpaceShip.Bullets.SimpleBullet;
+import donnu.zolotarev.SpaceShip.Bullets.SimpleBullet2;
+import donnu.zolotarev.SpaceShip.WeaponPos;
+
+public abstract class Guns {
+    protected int ATTACK_INTERVAL;
+    protected int shootDelay = 0;
+    protected boolean isShoot = false;
+
+    public void reload(){
+        shootDelay = 0;
+    }
+
+    public boolean shoot() {
+        shootDelay--;
+        if (shootDelay < 0){
+            shootDelay = ATTACK_INTERVAL;
+            return true;
+        }
+        return false;
+    }
+
+    public abstract void fire(WeaponPos weaponPos);
+
+    protected void GetNewBullet(final float x, final float y, final float direction) {
+        //    final SimpleBullet bullet;
+        if (direction == 0){
+            SimpleBullet.getBullet().init(x, y, direction);
+        } else {
+            SimpleBullet2.getBullet().init(x, y, direction);
+        }
+    }
+}
--- src/donnu/zolotarev/SpaceShip/Weapons/SimpleGun.java
+++ src/donnu/zolotarev/SpaceShip/Weapons/SimpleGun.java
@@ -1,28 +1,12 @@
 package donnu.zolotarev.SpaceShip.Weapons;
 
-import donnu.zolotarev.SpaceShip.Bullets.SimpleBullet;
-import donnu.zolotarev.SpaceShip.Bullets.SimpleBullet2;
 import donnu.zolotarev.SpaceShip.WeaponPos;
 
 public class SimpleGun extends Guns {
-    private static SimpleGun instance;
-    protected int ATTACK_INTERVAL;
-    protected int shootDelay = 0;
-    private boolean isShoot = false;
+
 
     public SimpleGun() {
-        instance = this;
-        ///
         ATTACK_INTERVAL = 5;
-    }
-
-    private void GetNewBullet(final float x, final float y, final float direction) {
-        //    final SimpleBullet bullet;
-        if (direction == 0){
-            SimpleBullet.getBullet().init(x, y, direction);
-        } else {
-            SimpleBullet2.getBullet().init(x, y, direction);
-        }
     }
 
     public void fire(WeaponPos weaponPos) {
@@ -30,16 +14,4 @@ public class SimpleGun extends Guns {
         GetNewBullet(weaponPos.x, weaponPos.y+5, weaponPos.anlge);
     }
 
-    public boolean shoot() {
-        shootDelay--;
-        if (shootDelay < 0){
-            shootDelay = ATTACK_INTERVAL;
-            return true;
-        }
-        return false;
-    }
-
-    public void reload(){
-        shootDelay = 0;
-    }
 }
